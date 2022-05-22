b5605a704916ea9ab3fb4c98d234f882ea3f3186
Kresh195 <KreshF5@mail.ru>
2022-05-22 19:01:43 +0700

    Processed the case of the absence of the book

--- main.py
+++ main.py
@@ -1,10 +1,40 @@
 import requests
+from pathlib import Path
+from urllib.error import HTTPError
 
-url = "https://tululu.org/txt.php?id=32168"
-response = requests.get(url)
-response.raise_for_status()
 
-filename = 'Пески Марса.txt'
+def creating_folder(folder):
+    Path(folder).mkdir(parents=True, exist_ok=True)
 
-with open(filename, 'wb') as file:
-    file.write(response.content)
+
+def downloading_books(folder):
+    books_downloading_url = "https://tululu.org/txt.php"
+    for book_id in range(1, 11):
+        params = {
+            'id': book_id
+        }
+        response = requests.get(books_downloading_url, params=params)
+        try:
+            check_for_redirect(response)
+            filename = f'{folder}/{book_id}.txt'
+            with open(filename, 'wb') as file:
+                file.write(response.content)
+        except requests.exceptions.HTTPError:
+            print(f'Книги {book_id} не существует')
+            continue
+
+
+def check_for_redirect(response):
+    if response.history:
+        raise requests.exceptions.HTTPError
+
+
+def main():
+    folder = 'books'
+
+    creating_folder(folder)
+    downloading_books(folder)
+
+
+if __name__ == "__main__":
+    main()
