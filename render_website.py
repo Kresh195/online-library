de84912fda6b4bc30e29f7dc17271547d94c6078
Kresh195 <KreshF5@mail.ru>
2023-03-26 18:47:45 +0700

    Added paginator

--- render_website.py
+++ render_website.py
@@ -6,29 +6,35 @@ from livereload import Server, shell
 from more_itertools import chunked
 
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
-
-os.makedirs("Pages", exist_ok=True)
-
-def render_page():
+def render_page(env):
     template = env.get_template('template.html')
     with open("media/books.json", "r", encoding="UTF8") as file:
         books = file.read()
     books = json.loads(books)
-    pages_books = chunked(books, 10)
+    pages_books = list(chunked(books, 10))
+    pages_count = len(pages_books)
     for page, page_books in enumerate(pages_books, 1):
         chunked_books = chunked(page_books, 2)
         rendered_page = template.render(
-            chunked_books=chunked_books
+            pages_count=pages_count,
+            chunked_books=chunked_books,
+            current_page=page
         )
-        with open(f"Pages//index{page}.html", "w", encoding="UTF8") as file:
+        with open(f"Pages/index{page}.html", "w", encoding="UTF8") as file:
             file.write(rendered_page)
 
 
-render_page()
+def main():
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+    os.makedirs("Pages", exist_ok=True)
+    render_page(env)
+
+
+if __name__ == "__main__":
+    main()
 # server = Server()
 # server.watch('*.html', render_page)
 # server.serve(root='.')
