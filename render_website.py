5828699f91a842aef408eaaba240db594512fdc9
Kresh195 <KreshF5@mail.ru>
2023-05-14 18:50:59 +0700

    Fixed livereload
    -Now using .env file for specify path to json file
    -Now on_reload() has no arguments(transfered to it from main() func)
    -Added python-dotenv to requirements.txt
    -Updated README

--- render_website.py
+++ render_website.py
@@ -3,14 +3,23 @@ import os
 import argparse
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
-from livereload import Server, shell
+from livereload import Server
 from more_itertools import chunked
+from dotenv import load_dotenv
 
 
-def on_reload(pages_books, pages_count, columns_number):
+def on_reload():
+    with open(os.path.join(json_path, 'books.json'), "r", encoding="UTF8") as file:
+        books_descriptions = json.load(file)
+    books_on_page = 10
+    pages_books = list(chunked(books_descriptions, books_on_page))
+    pages_count = len(pages_books)
+    columns_number = 2
     for page, page_books in enumerate(pages_books, 1):
         chunked_books = chunked(page_books, columns_number)
         render_page(pages_count, chunked_books, page)
+        print(page)
+    print("Detected")
 
 
 def render_page(pages_count, chunked_books, page):
@@ -29,28 +38,15 @@ def render_page(pages_count, chunked_books, page):
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Программа запускает локальный сайт')
-    parser.add_argument(
-        '--json_path',
-        default='media',
-        type=str,
-        help='Путь к JSON файлу с результатами'
-    )
-    args = parser.parse_args()
+    load_dotenv()
+    global json_path
+    json_path = os.getenv("JSON_PATH", default="media")
 
     os.makedirs("pages", exist_ok=True)
-    with open(os.path.join(args.json_path, 'books.json'), "r", encoding="UTF8") as file:
-        books_descriptions = json.load(file)
-    books_on_page = 10
-    pages_books = list(chunked(books_descriptions, books_on_page))
-    pages_count = len(pages_books)
-    columns_number = 2
-    for page, page_books in enumerate(pages_books, 1):
-        chunked_books = chunked(page_books, columns_number)
-        render_page(pages_count, chunked_books, page)
+    on_reload()
 
     server = Server()
-    server.watch('./template.html', on_reload(pages_books, pages_count, columns_number))
+    server.watch('*.html', on_reload)
     server.serve(root='.')
 
 
